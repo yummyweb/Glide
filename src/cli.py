9cfe3ea1ab65abece9f2b8e7feab7a8e5ecb340b
antriksh123 <antariksha.verma@pathways.in>
2021-06-02 15:43:47 +0530

    fix: change template to dict and convert it to str

--- src/cli.py
+++ src/cli.py
@@ -6,12 +6,12 @@ def init(project_name):
     """
     Creates a basic glide config file.
     """
-    template = """{
-    "project_name": """+ project_name + """
-}
-    """
+    template = {
+        "project_name": project_name
+    } 
+
     with open(project_name + '.glide', 'w') as fp:
-        fp.write(template)
+        fp.write(str(template))
     
 if __name__ =="__main__":
     init()

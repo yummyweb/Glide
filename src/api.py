18d86d887538dd20359e0a35a7e4051f7e928731
antriksh123 <antariksha.verma@pathways.in>
2021-06-02 19:27:41 +0530

    feat: deploy a site to netlify

--- src/api.py
+++ src/api.py
@@ -33,7 +33,10 @@ class Api:
         return (config['access_token'])
     
     def getHeaders(self) -> dict:
-        return {"Authorization": "Bearer " + self.getAccessToken()}
+        """
+        Returns the authorization headers
+        """
+        return {"Authorization": "Bearer " + self.getAccessToken(), "content-type": "application/json"}
     
     def getAllFilePathsForDirectory(self, dir):
         paths = []
@@ -45,6 +48,7 @@ class Api:
         BUF_SIZE = 65536
         hashes = {}
         for path in paths:
+            print("Hashing... " + path)
             sha1 = hashlib.sha1()
             with open(path, 'rb') as f:
                 while True:
@@ -54,24 +58,56 @@ class Api:
                     sha1.update(data)
 
                 file_hash = sha1.hexdigest()
+                print("Hashed path: " + file_hash)
                 relative_path = path.replace(directory, '')
-                hashes[relative_path] = file_hash
+                hashes[relative_path[1:]] = file_hash
         
         return hashes
+    
+    def invertDict(self, dict: dict) -> dict:
+        """
+        Inverts a given dict
+        """
+        return {v: k for k, v in dict.items()}
 
     def createSite(self) -> dict:
-        json_headers = self.getHeaders()
+        json_headers = {"Authorization": "Bearer " + self.getAccessToken()} 
         r = requests.post(self.netlifyBaseUrl + '/sites', headers=json_headers)
+        print("Site created with id: " + r.json()['id'])
         return r.json()
     
-    def deploy(self, directory):
+    def deploy(self, directory) -> tuple:
         json_headers = self.getHeaders()
         paths = self.getAllFilePathsForDirectory(directory)
         fileHashes = self.getShasum(paths, directory)
         site = self.createSite()
-        data = json.dumps({
-            "files": fileHashes,
-            "draft": True
-        })
-        r = requests.post(self.netlifyBaseUrl + '/sites/' + site['id'] + '/deploys', data, headers=json_headers)
+        data = {
+            "files": fileHashes
+        }
+        data_json = json.dumps(data)
+        r = requests.post(self.netlifyBaseUrl + '/sites/' + site['id'] + '/deploys', data=data_json, headers=json_headers)
+        print("Deploying to Netlify...")
+        return (r.json(), fileHashes)
+    
+    def uploadFile(self, directory):
+        accessToken = self.getAccessToken()
+        deployment, fileHashes = self.deploy(directory)
+        # Fetch the deployment id from deployment dict
+        deployId = deployment['id']
+        paths = self.getAllFilePathsForDirectory(directory)
+        fileUploadHeaders = {"Authorization": "Bearer " + accessToken, 'content-type': "application/octet-stream"}
+        for path in paths:
+            print('Uploading ' + path.replace(directory, '') + "...")
+            with open(path, 'rb') as file:
+                r = requests.put(self.netlifyBaseUrl + '/deploys/' + deployId + '/files' + path.replace(directory, ''), file, headers=fileUploadHeaders)
+                # Check if request was successful
+                if r.ok:
+                    print("File uploaded")
+                if not r.ok:
+                    print('\033[91m' + "ERROR:" + '\033[0m' + " Unable to upload file")
+                    print(r.content)
+    
+    def getSites(self) -> dict:
+        json_headers = self.getHeaders()
+        r = requests.get(self.netlifyBaseUrl + '/sites', headers=json_headers)
         return r.json()

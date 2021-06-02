85dfb0496fbf2543b567da1371563ea4d93d485e
antriksh123 <antariksha.verma@pathways.in>
2021-06-02 20:00:45 +0530

    feat: deploy from cli works

--- src/cli.py
+++ src/cli.py
@@ -1,4 +1,8 @@
 import click
+import glob
+from tabulate import tabulate
+from src.api import Api
+from src.deployment import Deployment
 
 @click.command()
 @click.argument('project_name')
@@ -16,6 +20,41 @@ def init(project_name):
 
     with open(project_name + '.glide', 'w') as fp:
         fp.write(str(template))
-    
+
+@click.command()
+@click.argument('directory')
+def deploy(directory):
+    """
+    Creates a basic glide config file.
+    """
+    for file in glob.glob("*.glide"):
+        with open(file, 'rb') as f:
+            deployment = Deployment(file.replace('.glide', ''), eval(f.read())['cloud_name'], directory)
+            deployment.deployToNetlify()
+
+@click.command()
+def sites():
+    """
+    Shows all available user sites.
+    """
+    for file in glob.glob("*.glide"):
+        with open(file, 'rb') as f:
+            api = Api(file.replace('.glide', ''))
+            table = []
+            for site in api.getSites():
+                table.append([site['id'], site['name'], site['url']])
+            
+            click.echo(tabulate(table, headers=['Site ID', 'Site Name', 'Site URL'], tablefmt="psql"))
+
+@click.group()
+@click.version_option()
+def cli():
+    pass
+
+cli.add_command(init)
+cli.add_command(deploy)
+cli.add_command(sites)
+
+
 if __name__ =="__main__":
-    init()
+    cli()

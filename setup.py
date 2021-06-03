cbf15a7480e011914fce4857e02c99f19d94d46c
antriksh123 <antariksha.verma@pathways.in>
2021-06-03 16:31:46 +0530

    feat: create setup.py for installing cli

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="glide",
+    version='0.1',
+    py_modules=['src.cli'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        glide=src.cli:cli
+    ''',
+)

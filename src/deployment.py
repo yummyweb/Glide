aec08b00173368461ff937dee5b1532e5b518490
antriksh123 <antariksha.verma@pathways.in>
2021-06-03 15:21:49 +0530

    fix: pass the deployToVercel function

--- src/deployment.py
+++ src/deployment.py
@@ -4,7 +4,10 @@ class Deployment:
     def __init__(self, deploymentName, directoryToDeploy):
         self.deploymentName = deploymentName
         self.directoryToDeploy = directoryToDeploy
+        self.api = Api(self.deploymentName)
     
     def deployToNetlify(self):
-        api = Api(self.deploymentName)
-        api.uploadFile(self.directoryToDeploy)
+        self.api.deployToNetlify(self.directoryToDeploy)
+    
+    def deployToVercel(self):
+        self.api.deployToVercel(self.directoryToDeploy)
